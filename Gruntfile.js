ee7ea93fa0c39d7b09fd130c64ae947fff942db4
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2015-12-15 16:32:27 +0100

    include popover

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,7 @@ module.exports = function (grunt) {
           'js/dropdown.js',
           // 'js/modal.js',
           'js/tooltip.js',
-          // 'js/popover.js',
+          'js/popover.js',
           'js/scrollspy.js',
           'js/tab.js',
           'js/affix.js'
